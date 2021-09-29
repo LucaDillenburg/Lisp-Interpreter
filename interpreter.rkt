c1178156ae965a084e46d92c33c48ae4445d4e4e
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2021-09-29 15:44:15 -0300

    Create simple parse, desugar and interp

--- interpreter.rkt
+++ interpreter.rkt
@@ -67,3 +67,143 @@
 ; 2. Desugar: expand syntax sugar (ExprS) into primitive expressions (ExprC)
 ; 3. Interp: execute the primitive expressions (ExprC) and return the value it got (Value)
 
+; 1. Parse: receive a quoted expression (s-expression) and parsers into an expression possibly with syntax sugar (ExprS)
+; Note that the quoted expression can be an array, a number, a symbol, etc
+(define (parse [s : s-expression]) : ExprS
+  (cond
+    [(s-exp-number? s) (numS (s-exp->number s))]
+    [(s-exp-symbol? s) (idS (s-exp->symbol s))] ; pode ser um símbolo livre nas definições de função
+    [(s-exp-list? s)
+     (let ([sl (s-exp->list s)])
+       (case (s-exp->symbol (first sl))
+         [(+) (plusS (parse (second sl)) (parse (third sl)))]
+         [(*) (multS (parse (second sl)) (parse (third sl)))]
+         [(-) (bminusS (parse (second sl)) (parse (third sl)))]
+         [(~) (uminusS (parse (second sl)))]
+         [(lambda) (lamS (s-exp->symbol (second sl)) (parse (third sl)))] ; definição
+         [(call) (appS (parse (second sl)) (parse (third sl)))]
+         [(if) (ifS (parse (second sl)) (parse (third sl)) (parse (fourth sl)))]
+         [(cons) (consS (parse (second sl)) (parse (third sl)))]
+         [(car) (carS (parse (second sl)))]
+         [(cdr) (cdrS (parse (second sl)))]
+         [else (error 'parse "invalid list input")]))]
+    [else (error 'parse "invalid input")]))
+
+; 2. Desugar: expand syntax sugar (ExprS) into primitive expressions (ExprC)
+(define (desugar [as : ExprS]) : ExprC
+  (type-case ExprS as
+    [numS    (n)        (numC n)]
+    [idS     (s)        (idC s)]
+    [lamS    (a b)      (lamC a (desugar b))]
+    [appS    (fun arg)  (appC (desugar fun) (desugar arg))]
+    [plusS   (l r)      (plusC (desugar l) (desugar r))]
+    [multS   (l r)      (multC (desugar l) (desugar r))]
+    [bminusS (l r)      (plusC (desugar l) (multC (numC -1) (desugar r)))]
+    [uminusS (e)        (multC (numC -1) (desugar e))]
+    [ifS     (c y n)    (ifC (desugar c) (desugar y) (desugar n))]
+    [consS   (b1 b2)    (consC (desugar b1) (desugar b2))]
+    [carS    (c)        (carC (desugar c))]
+    [cdrS    (c)        (cdrC (desugar c))]
+    ))
+
+; lookup changes its return type
+(define (lookup [varName : symbol] [env : Env]) : Value
+       (cond
+            [(empty? env) (error 'lookup (string-append (symbol->string varName) " não foi encontrado"))] ; livre (não definida)
+            [else (cond
+                    [(symbol=? varName (bind-name (first env)))   ; achou!
+                     (bind-val (first env))]
+                    [else (lookup varName (rest env))])]))        ; vê no resto
+
+; Primitive operators
+(define (num+ [l : Value] [r : Value]) : Value
+    (cond
+        [(and (numV? l) (numV? r))
+             (numV (+ (numV-n l) (numV-n r)))]
+        [else
+             (error 'num+ "Um dos argumentos não é número")]))
+
+(define (num* [l : Value] [r : Value]) : Value
+    (cond
+        [(and (numV? l) (numV? r))
+             (numV (* (numV-n l) (numV-n r)))]
+        [else
+             (error 'num* "Um dos argumentos não é número")]))
+
+
+; 3. Interp: execute the primitive expressions (ExprC) and return the value it got (Value)
+(define (interp [a : ExprC] [env : Env] ) : Value
+  (type-case ExprC a
+    [numC (n) (numV n) ]
+    [idC (n)  (lookup n env)]; cascading search, first in env then in store
+    [lamC (a b) (closV a b env) ]
+ 
+    ; application of function
+    [appC (f a)
+          (let ((closure (interp f env))
+                (argvalue (interp a env)))
+            (type-case Value closure
+              [closV (parameter body env)
+                     (interp body (extend-env (bind parameter argvalue) env))]
+              [else (error 'interp "operation app aplied to non-closure")]
+              ))]
+   
+    ;plusC
+    [plusC (l r)
+             (let ((left (interp l env))
+                   (right (interp r env)))
+               (if (numV? left)
+                 (if (numV? right)
+                     (num+ left right)
+                     (error 'interp "second argument of sum not a number value"))
+                 (error 'interp "first argument of sum not a number value"))
+               )]
+    ;multC
+    [multC (l r)
+           (let ( (left (interp l env))
+                  (right (interp r env)))
+             (if (numV? left)
+                 (if (numV? right)
+                     (num* left right)
+                     (error 'interp "second argument of multiplication not a number value"))
+                 (error 'interp "first argument of multiplication not a number value"))
+                 )]
+
+    ; ifC serializes
+    [ifC (c s n) (type-case Value (interp c env)
+                   [numV (value)
+                        (if (zero? value)
+                            (interp n env )
+                            (interp s env ))]
+                   [else (error 'interp "condition not a number")]
+                   )]
+
+    ; with lists
+    [consC (b1 b2) (let ( (car (interp b1 env))
+                          (cdr (interp b2 env)))
+                     (consV car cdr))]
+    [carC (c) (type-case Value (interp c env)
+                [consV (car cdr)
+                       car]
+                [else (error 'interp "car applied to non-cell")]
+                )]
+    [cdrC (c) (type-case Value (interp c env)
+                [consV (car cdr)
+                       cdr]
+                [else (error 'interp "cdr applied to non-cell")]
+                )]
+    ))
+
+; Facilitator
+(define (interpS [s : s-expression]) (interp (desugar (parse s)) mt-env))
+
+; Examples
+; (interpS '(+ 10 (call (lambda x (car x)) (cons 15 16))))
+; (interpS '(call (lambda x (+ x 5)) 8))
+; (interpS '(call (lambda f (call f (~ 32))) (lambda x (- 200 x))))
+
+; Tests
+; (test (interp (carC (consC (numC 10) (numC 20)))
+;              mt-env)
+;      (numV 10))
+
